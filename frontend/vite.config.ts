4c5e8cab880e430e48c517f4ba214eda4b31dcb0
Vladimir Avtsenov <vladimir.lsk.cool@gmail.com>
2025-02-28 02:42:20 +0300

    clear dist while building

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(() => ({
     ViteMinifyPlugin(),
   ],
   build: {
+    emptyOutDir: true,
     target: "esnext",
     // assetsInlineLimit: 400000, // inline fonts
   },
